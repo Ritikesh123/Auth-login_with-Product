e2d8eab1901e54608a96de3474a2eda8c8b3fc52
Ritikesh-Raj <ritikesh.raj04@gmail.com>
2023-08-11 15:21:14 +0530

    change-in-product

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,25 +1,16 @@
-import React, {useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { add } from '../store/cartSlice';
 import { fetchProducts } from '../store/productSlice';
 import { STATUSES } from '../store/productSlice';
-// import Navbar from './Navbar';
 
 const Products = () => {
     const dispatch = useDispatch();
     const { data: products, status } = useSelector((state) => state.product);
-    // const [products, setProducts] = useState([]);
 
     useEffect(() => {
         dispatch(fetchProducts());
-        // const fetchProducts = async () => {
-        //     const res = await fetch('https://fakestoreapi.com/products');
-        //     const data = await res.json();
-        //     console.log(data);
-        //     setProducts(data);
-        // };
-        // fetchProducts();
-    }, []);
+    }, [dispatch]); // Add dispatch to the dependency array
 
     const handleAdd = (product) => {
         dispatch(add(product));
@@ -32,9 +23,8 @@ const Products = () => {
     if (status === STATUSES.ERROR) {
         return <h2>Something went wrong!</h2>;
     }
+    
     return (
-        
-            
         <div className="productsWrapper">
             {products.map((product) => (
                 <div className="card" key={product.id}>
@@ -47,8 +37,6 @@ const Products = () => {
                 </div>
             ))}
         </div>
-       
-        
     );
 };
 
